5729ec37b43b7029938eda6d9c54ff79e17c15f8
Guilherme Dalia <daliaguilherme106@gmail.com>
2023-06-27 19:51:34 -0300

    atlz

---
+++ Site concessionária/site hb20/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slider').slick({
+      arrows: true,
+      dots: false,
+      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>'
+    });
+  });
+  
